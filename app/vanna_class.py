93fa02bc676771849319e39d9ccc35f7b93465f0
akreddy-p <ajay.reddy@solix.com>
2025-12-17 16:48:50 -0600

    fixed all dependency issues accross IDC and IDA

--- app/vanna_class.py
+++ app/vanna_class.py
@@ -51,7 +51,7 @@ class LLMModel_Chat(VannaBase):
 ## Any LLM Vanna
 class MyVanna(LLMModel_Chat, ChromaDB_VectorStore):
     document_store = {}
-    def __init__(self, model_source = "together", config=None):
+    def __init__(self, model_source = "openai", config=None):
         if config is None:
             config = {"path": current_app.config['VANNA_CHROMA']}
         current_app.logger.info(f"Current model source: {model_source}")
